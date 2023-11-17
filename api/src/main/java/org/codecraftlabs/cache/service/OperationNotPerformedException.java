c59801098c067027fd1ce3c892a0348a6ea2d8c2
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2023-11-18 00:31:23 +0100

    remove unused class

--- api/src/main/java/org/codecraftlabs/cache/service/OperationNotPerformedException.java
+++
@@ -1,7 +0,0 @@
-package org.codecraftlabs.cache.service;
-
-public class OperationNotPerformedException extends RuntimeException {
-    public OperationNotPerformedException(String message) {
-        super(message);
-    }
-}
