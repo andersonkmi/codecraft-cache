96dbd1ec15bdcbde2311a9becdccca2b229c00c8
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2023-11-19 11:46:29 +0100

    Include logic to consume the messages from SQS

--- cache-update-job/src/main/java/org/codecraftlabs/sqs/service/SQSConsumerService.java
+++ cache-update-job/src/main/java/org/codecraftlabs/sqs/service/SQSConsumerService.java
@@ -2,9 +2,13 @@ package org.codecraftlabs.sqs.service;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.codecraftlabs.cloudlift.sqs.SQSMessage;
+import org.codecraftlabs.cloudlift.sqs.SQSService;
 import org.codecraftlabs.sqs.util.JobConfiguration;
 
 import javax.annotation.Nonnull;
+import java.util.Optional;
+import java.util.Set;
 
 public class SQSConsumerService {
     private static final Logger logger = LogManager.getLogger(SQSConsumerService.class);
@@ -16,6 +20,20 @@ public class SQSConsumerService {
     }
 
     public void execute() throws AWSException {
+        if (jobConfiguration.getQueueUrl().isEmpty()) {
+            return;
+        }
+        String sqsUrl = jobConfiguration.getQueueUrl().get();
+        SQSService sqsService = SQSService.builder().build();
+        Optional<Set<SQSMessage>> messages = sqsService.receiveMessages(sqsUrl, 20);
+        if (messages.isEmpty()) {
+            return;
+        }
+        sqsService.deleteMessages(sqsUrl, messages.get());
+        messages.get().forEach(this::logMessage);
+    }
 
+    private void logMessage(@Nonnull SQSMessage message) {
+        logger.info("Message from the queue: " + message.body());
     }
 }
